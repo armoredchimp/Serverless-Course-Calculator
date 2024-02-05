e0c9b6178291d95f0d07050b61c0d2e1a8ee704c
armoredchimp <mattkasmir@gmail.com>
2024-02-05 11:36:03 -0800

    2/5

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+('use strict');
+
+import {
+  getColor,
+  bigColor,
+  invertedBigColor,
+  percentColor,
+  invertedPercentColor,
+} from './colors.js';
+
+//Selectors
+const courseUL = document.querySelector('.courses');
 let state = {
   courses: [], // Array for all courses
   displayCourses: [], // Array only for courses not hidden by toggle
@@ -11,6 +23,13 @@ function getID() {
   const currentId = state.id + 1;
   state.id = currentId;
   return currentId;
+}
+
+function removeCourse(id) {
+  state.courses = state.courses.filter((course) => course.id !== id);
+  state.displayCourses = state.displayCourses.filter(
+    (course) => course.id !== id
+  );
 }
 
 class Course {
@@ -31,6 +50,55 @@ class Course {
   remainingHours() {
     return Math.round((this.totalHours - this.completedHours()) * 100) / 100;
   }
+}
+
+class CourseItem extends Course {
+  constructor(name, totalHours, progress, id) {
+    super(name, totalHours, progress, id);
+    this.completedHours = this.completedHours();
+    this.remainingHours = this.remainingHours();
+    this.progressColor = percentColor(this.progress);
+    this.element = document.createElement('div');
+    this.element.className = 'course-line';
+    this.element.innerHTML = `
+    <div class="course-details">
+      <span style="color: ${this.progressColor};">${
+      this.progress
+    }%</span> of the ${this.totalHours}-hour ${
+      this.name
+    } course has been completed, which is roughly ${
+      this.completedHours
+    } hours. <span style="color: ${colorR};">${this.remainingHours.toFixed(
+      2
+    )}</span> hours remain for this course.
+    </div>
+    <div class="course-actions">
+      <button class="reset-button">Reset</button>
+      <button class="edit-button">Edit</button>
+    </div>
+  `;
+  }
+
+  delete() {
+    this.element.remove();
+    removeCourse(this.id);
+  }
+}
+
+function renderCourses() {
+  courseUL.innerHTML = '';
+
+  state.displayCourses.forEach((course) => {
+    const courseItem = new courseItem(
+      course.name,
+      course.totalHours,
+      course.progress,
+      course.id
+    );
+    const listItem = document.createElement('li');
+    listItem.appendChild(courseItem.element);
+    courseUL.appendChild(listItem);
+  });
 }
 
 sampleLink.addEventListener('click', () => {
