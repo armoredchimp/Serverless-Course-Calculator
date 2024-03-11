8a973fdd89607e461468a718b805a9c5a4591645
armoredchimp <mattkasmir@gmail.com>
2024-03-11 11:03:56 -0700

    3/11

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  API_URL:
+    'https://nc2ituc9o9.execute-api.us-west-2.amazonaws.com/dev/users/{id}',
+  API_PUT_URL:
+    'https://nc2ituc9o9.execute-api.us-west-2.amazonaws.com/dev/users/{id}',
+  API_TEST_URL:
+    'https://nc2ituc9o9.execute-api.us-west-2.amazonaws.com/dev/users/test',
+};
